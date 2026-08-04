ccddc1932703118baf8602fbf47d1dc96b75bec2
ahmedalmalky18 <88257232+9ozz@users.noreply.github.com>
2026-08-04 18:54:24 +0400

    add program entry point

---
+++ Backend/SparkyProject.API/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using SparkyProject.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// TODO: Register AppDbContext with connection string
+// builder.Services.AddDbContext<AppDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// TODO: Register JWT authentication
+// builder.Services.AddAuthentication(...).AddJwtBearer(...);
+// builder.Services.AddAuthorization();
+
+// TODO: register EmailService (see Services/EmailService.cs) for DI
+
+var app = builder.Build();
+
+// Middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// TODO: app.UseAuthentication();
+// TODO: app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
