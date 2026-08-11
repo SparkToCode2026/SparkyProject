80db15d35db2545a21e9beea291e76b47486a84e
ahmedalmalky18 <88257232+9ozz@users.noreply.github.com>
2026-08-12 01:55:38 +0400

    Models/Invoice.cs

--- Backend/SparkyProject.API/Models/Invoice.cs
+++ Backend/SparkyProject.API/Models/Invoice.cs
@@ -1,17 +1,20 @@
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace SparkyProject.API.Models;
 
 // Owner: Aisha Mubarak ALHashmi
-// TODO: add properties (PK, FKs, navigation properties) per the team ERD.
 public class Invoice
 {
+    [Key]
     public int InvoiceId { get; set; }
 
-    [ForeignKey("_booking")]
     public int BookingId { get; set; }
-    public Booking _booking { get; set; } = null!;
 
     public decimal TotalAmount { get; set; }
-    public DateTime IssuedAt { get; set; } = DateTime.Now;
+
+    public DateTime IssueDate { get; set; } = DateTime.Now;
+
+    public string Status { get; set; } = "Unpaid";
+
+    public Booking? Booking { get; set; }
 }
