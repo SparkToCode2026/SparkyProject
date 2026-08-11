bd5c71020446f907fe631aba954ad6ce1f811ccc
ahmedalmalky18 <88257232+9ozz@users.noreply.github.com>
2026-08-12 01:55:29 +0400

    Models/Hotel.cs

--- Backend/SparkyProject.API/Models/Hotel.cs
+++ Backend/SparkyProject.API/Models/Hotel.cs
@@ -3,18 +3,25 @@ using System.ComponentModel.DataAnnotations;
 namespace SparkyProject.API.Models;
 
 // Owner: Ibrahim Al Kindi
-// TODO: add properties (PK, FKs, navigation properties) per the team ERD.
 public class Hotel
 {
     [Key]
     public int HotelId { get; set; }
 
+    [Required]
+    public string Name { get; set; } = string.Empty;
 
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Phone { get; set; }
+
+    public int? UserId { get; set; }
 
     // 1:N Relationship with RoomType
-    public List<RoomType> RoomTypes { get; set; }
-
+    public List<RoomType> RoomTypes { get; set; } = new();
 
     // 1:N Relationship with Promotion
-    public List<Promotion> PromotionTypes { get; set; }
+    public List<Promotion> PromotionTypes { get; set; } = new();
 }
