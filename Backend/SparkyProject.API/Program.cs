99eea66824eab8f208c1bd4f79909213f699afca
Khalid Al Hashemi <khalidalhashemi199@gmail.com>
2026-08-17 14:13:28 +0400

    add swagger route in program.cs

--- Backend/SparkyProject.API/Program.cs
+++ Backend/SparkyProject.API/Program.cs
@@ -57,7 +57,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.RoutePrefix = string.Empty;
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SparkyProject API V1");
+    });
 }
 
 app.UseCors();
